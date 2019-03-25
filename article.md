8cbed1ffa4f21c5002c9767e29b02faa3ca58d58
timichal <michal@zlatkovsky.cz>
2019-03-25 17:23:37 +0100

    nahoz

---
+++ article.md
@@ -0,0 +1,20 @@
+title: "Komik, plynová princezna a čokoládový král. Kdo chce stanout v čele Ukrajiny?"
+perex: "Zkušení politici, novináři, komediální herec, kariérní špioni i váleční veteráni. Možná není náhoda, že se mají výsledky prvního kola ukrajinských voleb při troše štěstí vyhlásit na apríla. Spektrum kandidátů je opravdu široké: z 89 lidí nakonec splnilo podmínky rekordních 44, pět pak kandidaturu vzdalo. Průzkumům vévodil týdny před volbami komik Volodymyr Zelenskyj, druhé kolo ale jisté nemá, ani jeden z tandemu Julia Tymošenková a Petro Porošenko se nevzdává. Přinášíme přehled hlavních kandidátů na prezidenta země, která je už pět let ve válce."
+published: "26. března 2019"
+coverimg: https://www.irozhlas.cz/sites/default/files/styles/zpravy_fotogalerie_medium/public/images/03456343.jpeg?itok=xkVeTk-v
+coverimg_note: "Petro Porošenko • Reuters"
+styles: []
+libraries: [] #jquery, d3, highcharts, datatables
+options: [noheader] #wide, noheader (, nopic)
+---
+V květnu 2014 vyhrál prezidentské volby s téměř 55 procenty Petro Porošenko už v prvním kole. Ukrajinci volili hlavu státu po revoluci a sesazení prezidenta Viktora Janukovyče, několik týdnů po ruském obsazení Krymu a v situaci zuřícího válečného konfliktu na východě země.
+
+Vítěz letošních voleb zdědí zemi, která udělala během pěti let kroky evropským směrem, nedokázala ale zatočit s všudypřítomnou korupcí, a i když se konflikt v Donbasu změnil v zamrzlý, musí se Ukrajina dál vypořádat se snahami o destabilizaci země. Porošenkovu cestu k obhajobě kazí kromě jiného i prostá statistika: od osamostatnění Ukrajiny v roce 1991 dokázal ve volbách obhájit svou funkci jen jediný prezident.
+
+<div id="kandidati"></div>
+<p></p>
+
+## Předvolební průzkumy
+Tři průzkumy z poloviny března přisuzovaly vítězství v prvním kole Zelenskému s náskokem 6-8 procentních bodů nad druhou Tymošenkovou. Za ní následuje současný prezident Porošenko a kolem hranice od 5 do 10 procent se v průzkumech drží kandidáti Bojko a Hrycenko. Vůli jít k volbám vyjádřilo podle průzkumu Jaremenkova institutu pro sociální výzkum až 82 procent lidí. Ve volbách před pěti lety odvolilo v prvním kole 60 procent oprávněných voličů.
+
+<div class="infogram-embed" data-id="_/DOQMMxcpNDQaBKJGLiUg" data-type="interactive" data-title="Průzkum: první kolo ukrajinských prezidentských voleb"></div><script>!function(e,t,s,i){var n="InfogramEmbeds",o=e.getElementsByTagName("script")[0],d=/^http:/.test(e.location)?"http:":"https:";if(/^\/{2}/.test(i)&&(i=d+i),window[n]&&window[n].initialized)window[n].process&&window[n].process();else if(!e.getElementById(s)){var r=e.createElement("script");r.async=1,r.id=s,r.src=i,o.parentNode.insertBefore(r,o)}}(document,0,"infogram-async","https://e.infogram.com/js/dist/embed-loader-min.js");</script>
---
+++ js/script.js
@@ -0,0 +1,164 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/no-danger */
+import "./targetblank"; // pro otvírání odkazů v novém okně
+import { h, render, Component } from "preact";
+/** @jsx h */
+import { getAge } from "./datehelper";
+
+const rootUrl = "https://data.irozhlas.cz/uk-prezident-kandidati";
+//const rootUrl = "http://localhost:8000/sk-prezident-kandidati";
+
+const DisplayBox = ({ candidate, handleClick }) => {
+  const imgName = `${candidate.name.toLowerCase().replace(" ", "_")}.jpg`;
+
+  return (
+    <div>
+      <div className="displaybox-overlay" />
+      <div className="displaybox">
+        <div className="displaybox-top">
+          <div className="displaybox-img-container">
+            <img className="displaybox-img" alt={candidate.name} src={`${rootUrl}/fotky/${imgName}`} />
+          </div>
+          <div className="displaybox-header">
+            <div className="displaybox-name">{candidate.name}</div>
+            <div className="displaybox-blurb">{candidate.blurb}</div>
+            <div className="displaybox-age">{`${getAge(candidate.dob)} let`}</div>
+          </div>
+          <div role="button" tabIndex="0" className="displaybox-close" onClick={() => handleClick("close")}>✕</div>
+        </div>
+        <div className="displaybox-desc" dangerouslySetInnerHTML={{ __html: candidate.desc }} />
+        <div className="displaybox-nav">
+          <div role="button" tabIndex="0" className="displaybox-nav-arrow" onClick={() => handleClick("left")}>‹</div>
+          <div role="button" tabIndex="0" className="displaybox-nav-arrow" onClick={() => handleClick("right")}>›</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+const Entry = ({ candidate, id, handleClick }) => {
+  const imgName = `${candidate.name.toLowerCase().replace(" ", "_")}.jpg`;
+
+  return (
+    <div className="entry" role="button" tabIndex="0" onClick={() => handleClick(id)}>
+      <img className="entry-img" alt={candidate.name} src={`${rootUrl}/fotky/${imgName}`} />
+      <div className="entry-info">
+        <div className="entry-name">{candidate.name}</div>
+        <div className="entry-blurb">{candidate.blurb}</div>
+        <div className="entry-age">{`${getAge(candidate.dob)} let`}</div>
+        <div className="entry-desc">{candidate.desc.split("<p>")[1]}</div>
+      </div>
+    </div>
+  );
+};
+
+class Tablo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibleBox: false,
+      selectedCand: null,
+      data: [],
+    };
+    this.handleEntryClick = this.handleEntryClick.bind(this);
+    this.handleBoxClick = this.handleBoxClick.bind(this);
+    this.handleKeys = this.handleKeys.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadData();
+    document.documentElement.addEventListener("keyup", this.handleKeys);
+    document.documentElement.addEventListener("mouseup", this.handleMouseUp);
+  }
+
+  loadData() {
+    const xhr = new XMLHttpRequest();
+    const url = `${rootUrl}/data/data.json`;
+    xhr.open("get", url, true);
+    xhr.onload = () => {
+      this.setState({ data: JSON.parse(xhr.responseText) });
+    };
+    xhr.send();
+  }
+
+  handleKeys(event) {
+    const { visibleBox, selectedCand, data } = this.state;
+    if (visibleBox) {
+      if (event.key === "Escape") {
+        this.setState({
+          visibleBox: false,
+        });
+        document.documentElement.style.overflow = "auto";
+      } else if (event.key === "ArrowRight") {
+        this.setState({
+          selectedCand: (selectedCand + 1) % data.length,
+        });
+      } else if (event.key === "ArrowLeft") {
+        this.setState({
+          selectedCand: selectedCand === 0 ? data.length - 1 : selectedCand - 1,
+        });
+      }
+    }
+  }
+
+  handleMouseUp(event) {
+    const { visibleBox } = this.state;
+    if (visibleBox) {
+      const box = document.getElementsByClassName("displaybox")[0];
+      if (event.target !== box && !box.contains(event.target)) {
+        this.setState({
+          visibleBox: false,
+        });
+        document.documentElement.style.overflow = "auto";
+      }
+    }
+  }
+
+  handleEntryClick(id) {
+    this.setState({
+      visibleBox: true,
+      selectedCand: id,
+    });
+    document.documentElement.style.overflow = "hidden";
+  }
+
+  handleBoxClick(action) {
+    const { selectedCand, data } = this.state;
+    if (action === "close") {
+      this.setState({
+        visibleBox: false,
+      });
+      document.documentElement.style.overflow = "auto";
+    } else if (action === "right") {
+      this.setState({
+        selectedCand: (selectedCand + 1) % data.length,
+      });
+    } else if (action === "left") {
+      this.setState({
+        selectedCand: selectedCand === 0 ? data.length - 1 : selectedCand - 1,
+      });
+    }
+  }
+
+  render() {
+    const { visibleBox, selectedCand, data } = this.state;
+    const displayBox = visibleBox
+      ? <DisplayBox candidate={data[selectedCand]} handleClick={this.handleBoxClick} />
+      : null;
+
+    return (
+      <div id="tablo">
+        {displayBox}
+        {data.map((candidate, candId) => (
+          <Entry candidate={candidate} id={candId} handleClick={this.handleEntryClick} />
+        ))}
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+render(<Tablo />, document.getElementById("kandidati"));
